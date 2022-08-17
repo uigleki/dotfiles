fa86f6c9c4fa916457526f846a066d285437a22a
ray <rraayy246@gmail.com>
2022-08-17 22:18:28 +0800

    a

--- .config/qtile/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-dunst &
-fcitx5 &
-feh --bg-fill --no-fehbg --randomize ~/a/pixra/bimple &
-nm-applet &
-redshift-gtk &
-setxkbmap -option 'ctrl:nocaps' &
-udiskie --smart-tray &
