7d221fe1bd9589759b87369df9d914f4a92322cf
ray <rraayy246@gmail.com>
2022-08-24 13:26:36 +0800

    org capture

--- .config/emacs/lisp/init-org.el
+++ .config/emacs/lisp/init-org.el
@@ -5,6 +5,16 @@
   (:map org-mode-map ("C-c v" . visible-mode))
   :custom
   (org-agenda-files '("~/org"))
-  (org-default-notes-file "~/org/todo.org"))
+  (org-default-notes-file "~/org/todo.org")
+  (org-log-done 'time)                  ; 记录完成时间
+  (org-capture-templates
+   '(("t" "Todo" entry (file+headline "~/org/todo.org" "Tasks")
+      "* TODO %^{任务名}\n%u\n%a")
+     ("d" "Diary" entry (file+datetree "~/org/diary.org")
+      "* %U - %^{heading}\n%?")
+     ("i" "Inbox" entry (file "~/org/inbox.org")
+      "* %U - %^{heading} %^g\n%?")
+     ("n" "Notes" entry (file "~/org/note.org")
+      "* %^{heading} %t %^g\n%?"))))
 
 (provide 'init-org)
