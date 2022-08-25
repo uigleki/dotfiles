2322269a7bca36c433b2c8f92d62cac721ffcb63
ray <rraayy246@gmail.com>
2022-08-25 20:06:54 +0800

    init magit

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -20,6 +20,7 @@
 (require 'init-corfu)                   ; 插入模式补全
 (require 'init-consult)                 ; 全局搜索补全
 (require 'init-diff-hl)                 ; git 差异显示
+(require 'init-magit)                   ; git 管理
 
 ;; 语言
 (require 'init-go-translate)            ; 语言翻译
