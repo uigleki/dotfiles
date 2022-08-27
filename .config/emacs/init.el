c16d49d2cc0e5d80262aabdffd144853ac39c0a8
ray <rraayy246@gmail.com>
2022-08-27 13:58:57 +0800

    a

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -13,25 +13,25 @@
 (require 'init-keymap)                  ; 按键映射
 
 ;; 补全
-(require 'init-recentf)                 ; 最近文件
-(require 'init-undo-tree)               ; 撤销树
+(require 'init-saveplace)               ; 记住光标位置
 (require 'init-which-key)               ; 显示按键绑定
+(require 'init-undo-tree)               ; 撤销树
+(require 'init-recentf)                 ; 最近文件
 (require 'init-orderless)               ; 补全候选排序
 (require 'init-vertico)                 ; 命令缓冲区补全
 (require 'init-corfu)                   ; 插入模式补全
 (require 'init-consult)                 ; 全局搜索补全
 (require 'init-diff-hl)                 ; git 差异显示
 (require 'init-magit)                   ; git 管理
-(require 'init-saveplace)               ; 记住光标位置
 
 ;; 语言
 (require 'init-go-translate)            ; 语言翻译
 (require 'init-eglot)                   ; 语言客户端
 (require 'init-flymake)                 ; 语法检查
 (require 'init-org)                     ; org 模式
+(require 'init-tree-sitter)             ; 语法树
 (require 'init-quickrun)                ; 快速执行缓冲区
 (require 'init-realgud)                 ; 调试器
-(require 'init-tree-sitter)             ; 语法树
 
 ;; 语言包
 (use-package markdown-mode)
