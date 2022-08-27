9600d8bfe6b76f8216549abd5d9c2a6375e6a242
ray <rraayy246@gmail.com>
2022-08-28 01:52:33 +0800

    a

--- etc/profile.d/emacs.sh
+++ etc/profile.d/emacs.sh
@@ -1,4 +1,2 @@
 # 在图形环境下，emacs 自启动，使用真彩色
-if [ "$XDG_SESSION_TYPE" != 'tty' ] && [ -x "$(command -v emacs)" ]; then
-    COLORTERM=truecolor emacs --daemon &>/dev/null &
-fi
+(COLORTERM=truecolor emacsclient -nt &>/dev/null &)
