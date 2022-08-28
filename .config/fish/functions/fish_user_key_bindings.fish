057b028fb07e9281e6df4697c6233fa755268cbd
ray <rraayy246@gmail.com>
2022-08-29 01:04:43 +0800

    a

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,8 +1,8 @@
-# 启用 fzf 快捷键：
+# 启用 fzf 快捷键
 #
-# ctrl + t 选择文件
-# ctrl + r 选择历史记录
-#  alt + c 目录跳转
+# ctrl + t => 选择文件
+# ctrl + r => 选择历史记录
+# alt + c => 目录跳转
 
 function fish_user_key_bindings
     fzf_key_bindings
