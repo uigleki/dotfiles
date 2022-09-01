b3f90201ad4561941579acb892f895ee9733afda
ray <rraayy246@gmail.com>
2022-09-01 18:16:06 +0800

    a

--- .config/emacs/lisp/init-consult.el
+++ .config/emacs/lisp/init-consult.el
@@ -1,14 +1,16 @@
 ;;; 命令缓冲区跳转预览
+;;
+;; 在命令缓冲区的候选列表中，预览选中的选项。
 
 (use-package consult
   :bind
-  ([remap apropos-command] . consult-apropos)
-  ([remap goto-line] . consult-goto-line)
-  ([remap imenu] . consult-imenu)
-  ([remap project-find-regexp] . consult-git-grep)
-  ([remap project-switch-to-buffer] . consult-project-buffer)
-  ([remap switch-to-buffer] . consult-buffer)
-  ([remap yank-pop] . consult-yank-pop)
+  ([remap apropos-command] . consult-apropos)      ; key: C-h a
+  ([remap goto-line] . consult-goto-line)          ; key: M-g g
+  ([remap imenu] . consult-imenu)                  ; key: C-c i
+  ([remap project-find-regexp] . consult-git-grep) ; key: C-c p g
+  ([remap project-switch-to-buffer] . consult-project-buffer) ; key: C-c p b
+  ([remap switch-to-buffer] . consult-buffer) ; key: C-c b
+  ([remap yank-pop] . consult-yank-pop)       ; key: M-y
   (:map mode-specific-map
         ("f" . consult-find)
         ("q" . consult-ripgrep)))
--- .config/emacs/lisp/init-eglot.el
+++ .config/emacs/lisp/init-eglot.el
@@ -1,4 +1,7 @@
 ;;; 语言服务器协议的客户端
+;;
+;; M-. => 跳转到符号的定义
+;; M-? => 查找符号调用
 
 (use-package eglot
   :hook ((python-mode
