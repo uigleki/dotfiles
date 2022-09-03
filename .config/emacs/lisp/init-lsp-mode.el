c885de184359d730f2f427c90a04ddc7de99cb66
ray <rraayy246@gmail.com>
2022-09-03 15:31:25 +0800

    a

--- .config/emacs/lisp/init-lsp-mode.el
+++ .config/emacs/lisp/init-lsp-mode.el
@@ -6,11 +6,13 @@
     sh-mode) . lsp)
   (lsp-mode . lsp-enable-which-key-integration)
   :custom
-  (lsp-keymap-prefix "C-c l")
+  (lsp-keymap-prefix "C-c l")           ; 前缀键
   :commands lsp)
 
 (when *is-graphic*
   (use-package lsp-ui
+    :custom
+    (lsp-ui-sideline-show-code-actions t) ; 边线显示代码操作
     :commands lsp-ui-mode)
 
   (use-package dap-mode))
