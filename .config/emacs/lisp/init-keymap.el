0ed7c46878742e9418fd5f8eaf46d44f1dfc9cec
ray <rraayy246@gmail.com>
2022-09-04 04:24:35 +0800

    meow and xfk

--- .config/emacs/lisp/init-keymap.el
+++ .config/emacs/lisp/init-keymap.el
@@ -1,7 +1,9 @@
 ;;; 按键绑定
+;;
+;; 使用模态编辑，使用李杀的飞键键位，但是飞键模式有问题，所以用兼容性超好的猫态编辑模式实现
+;; 具体是加载飞键的函数，由猫态模式去调用
 
-;; 使用模态编辑
+(require 'init-xah-fly-keys)            ; 李杀飞键
 (require 'init-meow)                    ; 猫态编辑
-;; (require 'init-xah-fly-keys)            ; 杀飞键
 
 (provide 'init-keymap)
--- .config/emacs/lisp/init-xah-fly-keys.el
+++ .config/emacs/lisp/init-xah-fly-keys.el
@@ -1,10 +1,10 @@
-;;; 模态编辑：杀飞键
+;;; 模态编辑：李杀飞键
+;;
+;; 只加载飞键的函数，因为飞键模式本身有很多问题，而且和其他模式冲突
 
 (use-package xah-fly-keys
-  :bind
-  ("<escape>" . xah-fly-command-mode-activate)
-  :config
-  (xah-fly-keys-set-layout "qwerty")
-  (xah-fly-keys 1))
+    :custom
+    (xah-fly-use-control-key nil) ; 禁用对 Emacs 控件键绑定的更改
+    (xah-fly-use-meta-key nil)) ; 禁用对 Emacs 元键绑定的更改
 
 (provide 'init-xah-fly-keys)
