b34b5f10a237456bc0995779d267282631069419
ray <rraayy246@gmail.com>
2022-09-03 16:58:02 +0800

    强制代码缩进

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -23,25 +23,25 @@
 (require 'init-keymap)                  ; 按键绑定
 (require 'init-dashboard)               ; 欢迎界面
 
-;; 补全
 (require 'init-saveplace)               ; 记住光标位置
+(require 'init-aggressive-indent)       ; 强制代码缩进
 (require 'init-which-key)               ; 显示按键绑定
 (require 'init-undo-tree)               ; 撤销树
 (require 'init-recentf)                 ; 最近文件
 (require 'init-orderless)               ; 补全候选排序
-(require 'init-vertico)                 ; 命令缓冲区补全
 (require 'init-corfu)                   ; 编辑缓冲区补全
+(require 'init-vertico)                 ; 命令缓冲区补全
 (require 'init-consult)                 ; 跳转预览
-(require 'init-diff-hl)                 ; git 差异显示
 (require 'init-magit)                   ; git 管理
+(require 'init-diff-hl)                 ; git 差异显示
 (require 'init-dirvish)                 ; 文件管理器
 (require 'init-keyfreq)                 ; 统计命令频率
 
 ;; 语言
-(require 'init-go-translate)            ; 语言翻译
-(require 'init-flymake)                 ; 语法检查
 (require 'init-org)                     ; org 模式
 (require 'init-tree-sitter)             ; 语法树
+(require 'init-go-translate)            ; 语言翻译
+(require 'init-flymake)                 ; 语法检查
 (require 'init-quickrun)                ; 执行缓冲区
 (require 'init-lsp-mode)                ; 语言客户端
 ;; (require 'init-eglot)                   ; 语言客户端
---
+++ .config/emacs/lisp/init-aggressive-indent.el
@@ -0,0 +1,6 @@
+;;; 强制代码缩进
+
+(use-package aggressive-indent
+  :config (global-aggressive-indent-mode 1))
+
+(provide 'init-aggressive-indent)
