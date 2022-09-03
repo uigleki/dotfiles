db2024ff72550e703ddd083bcfb239c0edf9a03e
ray <rraayy246@gmail.com>
2022-09-03 13:29:06 +0800

    a

--- .config/emacs/lisp/init-which-key.el
+++ .config/emacs/lisp/init-which-key.el
@@ -1,6 +1,10 @@
 ;;; 显示可用按键绑定
 
 (use-package which-key
-  :config (which-key-mode 1))
+  :custom
+  ;; 守护程序下，解决最后一行被隐藏的问题
+  (which-key-allow-imprecise-window-fit nil)
+  :config
+  (which-key-mode 1))
 
 (provide 'init-which-key)
