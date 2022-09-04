ab1329c0472108272e782a1d237eba46c43fb751
ray <rraayy246@gmail.com>
2022-09-04 23:51:00 +0800

    a

--- .config/emacs/lisp/init-xah-fly-keys.el
+++ .config/emacs/lisp/init-xah-fly-keys.el
@@ -3,8 +3,9 @@
 ;; 只加载飞键的函数，因为飞键模式本身有很多问题，而且和其他模式冲突
 
 (use-package xah-fly-keys
-    :custom
-    (xah-fly-use-control-key nil)      ; 禁用对 Emacs 控件键绑定的更改
-    (xah-fly-use-meta-key nil))        ; 禁用对 Emacs 元键绑定的更改
+  :custom
+  ;; 不要删除 emacs 的默认快捷键
+  (xah-fly-use-control-key nil)
+  (xah-fly-use-meta-key nil))
 
 (provide 'init-xah-fly-keys)
