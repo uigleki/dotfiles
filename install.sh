073ec93a7829481e165d69a928d41d99cad66128
Ray <rraayy246@gmail.com>
2024-10-28 14:35:29 +0800

    better install

--- install.sh
+++ install.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 
-SYSTEM="$(uname -m)-$(uname -s | tr '[:upper:]' '[:lower:]')"
-CONFIG_GIT="https://gitlab.com/uigleki/dotfiles.git"
-CONFIG_DIR="$HOME/.config/home-manager"
-
 set -eux
+
+readonly SYSTEM="$(uname -m)-linux"
+readonly CONFIG_GIT="https://gitlab.com/uigleki/dotfiles.git"
+readonly CONFIG_DIR="$HOME/.config/home-manager"
+readonly NIX_PATHS=(
+    "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh"
+    "$HOME/.nix-profile/etc/profile.d/nix.sh"
+)
 
 setup_config() {
     local tmp_dir
     tmp_dir=$(mktemp -d)
+
     git clone --depth=1 "$CONFIG_GIT" "$tmp_dir"
     cp -r "$tmp_dir/.config" "$HOME"
     rm -rf "$tmp_dir"
 
     sed -i "s/SYSTEM_PLACEHOLDER/$SYSTEM/g; s/USERNAME_PLACEHOLDER/$USER/g" \
         "$CONFIG_DIR/flake.nix" "$CONFIG_DIR/home.nix"
+
     sudo find /etc -type f -name "*shrc.backup-before-nix" -delete
 }
 
@@ -23,10 +29,6 @@ install_nix() {
         echo "n\ny\n" | sh <(curl -L https://nixos.org/nix/install) --daemon
     fi
 
-    NIX_PATHS=(
-        "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh"
-        "$HOME/.nix-profile/etc/profile.d/nix.sh"
-    )
     for nix_path in "${NIX_PATHS[@]}"; do
         if [ -e "$nix_path" ]; then
             source "$nix_path"
@@ -39,13 +41,17 @@ install_nix() {
 }
 
 setup_shell() {
-    sudo passwd -d "$USER"
     local zsh_path
     zsh_path=$(command -v zsh)
-    if ! grep -q "$zsh_path" /etc/shells; then
-        echo "$zsh_path" | sudo tee -a /etc/shells
+
+    if [ "$SHELL" != "$zsh_path" ]; then
+        if ! grep -q "^${zsh_path}$" /etc/shells; then
+            echo "$zsh_path" | sudo tee -a /etc/shells
+        fi
+
+        sudo passwd -d "$USER"
+        chsh -s "$zsh_path"
     fi
-    chsh -s "$zsh_path"
 }
 
 main() {
