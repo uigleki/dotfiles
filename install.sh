18e7349129d1f0867ae182d513b358a2b897e4df
Ray <rraayy246@gmail.com>
2024-10-31 00:28:47 +0800

    test

--- install.sh
+++ install.sh
@@ -6,60 +6,50 @@ SYSTEM="$(uname -m)-linux"
 readonly SYSTEM
 readonly CONFIG_GIT="https://github.com/uigleki/dotfiles.git"
 readonly CONFIG_DIR="$HOME/.config/home-manager"
-readonly NIX_PATHS=(
-    "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh"
-    "$HOME/.nix-profile/etc/profile.d/nix.sh"
-)
 
-setup_config() {
+clone_and_setup_config() {
     local tmp_dir
     tmp_dir=$(mktemp -d)
+    trap 'rm -rf "$tmp_dir"' EXIT
 
     git clone --depth=1 "$CONFIG_GIT" "$tmp_dir"
     cp -r "$tmp_dir/.config" "$HOME"
-    rm -rf "$tmp_dir"
 
     sed -i "s/SYSTEM_PLACEHOLDER/$SYSTEM/g; s/USERNAME_PLACEHOLDER/$USER/g" \
         "$CONFIG_DIR/flake.nix" "$CONFIG_DIR/home.nix"
 }
 
 install_nix() {
-    if ! command -v nix >/dev/null 2>&1; then
-        sudo find /etc -type f -name "*shrc.backup-before-nix" -delete
-
+    if ps -p 1 -o comm= | grep -q systemd && [ "$(cat /sys/fs/selinux/enforce 2>/dev/null)" != "1" ]; then
         printf "n\ny\n" | sh <(curl -L https://nixos.org/nix/install) --daemon
-
-        for nix_path in "${NIX_PATHS[@]}"; do
-            if [ -e "$nix_path" ]; then
-                # shellcheck source=/dev/null
-                source "$nix_path"
-                break
-            fi
-        done
+        source "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh"
+    else
+        sh <(curl -L https://nixos.org/nix/install) --no-daemon
+        source "$HOME/.nix-profile/etc/profile.d/nix.sh"
     fi
+}
 
+setup_nix() {
+    command -v nix >/dev/null 2>&1 || install_nix
     nix run nixpkgs#home-manager -- switch -b backup
     nix store gc
 }
 
-setup_shell() {
+set_default_shell() {
     local zsh_path
     zsh_path=$(command -v zsh)
 
     if [ "$SHELL" != "$zsh_path" ]; then
-        if ! grep -q "^${zsh_path}$" /etc/shells; then
-            echo "$zsh_path" | sudo tee -a /etc/shells
-        fi
-
+        grep -q "^${zsh_path}$" /etc/shells || echo "$zsh_path" | sudo tee -a /etc/shells
         sudo passwd -d "$USER"
         chsh -s "$zsh_path"
     fi
 }
 
 main() {
-    setup_config
-    install_nix
-    setup_shell
+    clone_and_setup_config
+    setup_nix
+    set_default_shell
 }
 
 main
