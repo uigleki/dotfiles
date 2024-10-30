543ed60d286d0d9686175f56a195d03c5a0384ab
Ray <rraayy246@gmail.com>
2024-10-30 17:27:56 +0800

    test

--- .devcontainer/local-features/nix/install.sh
+++ .devcontainer/local-features/nix/install.sh
@@ -4,9 +4,9 @@ USERNAME="${USERNAME:-"vscode"}"
 
 set -eux
 
+runuser -l "$USERNAME" -c "sh <(curl -L https://nixos.org/nix/install) --no-daemon"
+
 runuser -l "$USERNAME" <<'EOF'
-sh <(curl -L https://nixos.org/nix/install) --no-daemon
-mkdir -p ~/.config/nix
-echo 'experimental-features = nix-command flakes' > ~/.config/nix/nix.conf
-nix profile install nixpkgs#nil
+install -Dm644 <(echo 'experimental-features = nix-command flakes') ~/.config/nix/nix.conf
+nix profile install nixpkgs#nil nixpkgs#nixpkgs-fmt
 EOF
