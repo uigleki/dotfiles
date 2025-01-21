c2f294c93093f7ae27afce37a8defc9226d1bb41
Ray <rraayy246@gmail.com>
2025-01-21 07:42:20 +0000

    feat: add nix-direnv development environment

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ nil nixfmt shellcheck shfmt ];
+
+          shellHook = ''
+            echo "🚀 Development environment activated"
+          '';
+        };
+      });
+}
