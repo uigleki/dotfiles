4b07cedd16d646312473d21df9b079fc4a0fda02
Ray <rraayy246@gmail.com>
2025-09-12 17:46:05 +0800

    a

--- hosts/vm-arm/disk-config.nix
+++ hosts/vm-arm/disk-config.nix
@@ -18,7 +18,6 @@
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
-              mountOptions = [ "umask=0077" ];
             };
           };
           root = {
