11a15b02a3c1432819053edb10d15d76986b64b9
Ray <rraayy246@gmail.com>
2025-09-18 07:28:35 +0000

    test opt

--- nixos/disk-config.nix
+++ nixos/disk-config.nix
@@ -1,44 +1,66 @@
-{ lib, inputs, ... }:
+{
+  config,
+  lib,
+  inputs,
+  ...
+}:
 let
+  cfg = config.myModules.diskConfig;
+
   subvol = mountpoint: {
+    inherit mountpoint;
     mountOptions = [
       "compress=zstd"
       "noatime"
     ];
-    inherit mountpoint;
   };
 in
 {
   imports = [ inputs.disko.nixosModules.disko ];
 
-  services.btrfs.autoScrub.enable = true;
+  options.myModules.diskConfig = {
+    enable = lib.mkEnableOption "Enable disk configuration." // {
+      default = true;
+    };
 
-  disko.devices = {
-    disk.main = {
-      device = lib.mkDefault "/dev/sda";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            size = "500M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [ "umask=0077" ];
+    device = lib.mkOption {
+      type = lib.types.str;
+      default = "/dev/sda";
+      example = "/dev/nvme0n1";
+      description = "Device to use for disk configuration.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.btrfs.autoScrub.enable = true;
+
+    disko.devices = {
+      disk.main = {
+        device = cfg.device;
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "500M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
             };
-          };
-          root = {
-            size = "100%";
-            content = {
-              type = "btrfs";
-              extraArgs = [ "-f" ];
-              subvolumes = {
-                "root" = subvol "/";
-                "home" = subvol "/home";
-                "nix" = subvol "/nix";
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "root" = subvol "/";
+                  "home" = subvol "/home";
+                  "nix" = subvol "/nix";
+                };
               };
             };
           };
