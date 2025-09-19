38572a5ee614b2a5f8586680465a9cbdae2f75ed
Ray <rraayy246@gmail.com>
2025-09-19 17:31:17 +0000

    a

--- hosts/akira/default.nix
+++ hosts/akira/default.nix
@@ -2,11 +2,5 @@
 {
   imports = [ ../../nixos/wsl.nix ];
 
-  myModules = {
-    boot.enable = false;
-    diskConfig.enable = false;
-    network.enable = false;
-  };
-
   home-manager.users.${user.name}.myModules.dev.enable = true;
 }
--- nixos/wsl.nix
+++ nixos/wsl.nix
@@ -4,4 +4,10 @@
 
   wsl.enable = true;
   programs.nix-ld.enable = true;
+
+  myModules = {
+    boot.enable = false;
+    diskConfig.enable = false;
+    network.enable = false;
+  };
 }
