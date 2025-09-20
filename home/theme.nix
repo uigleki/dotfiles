373eb6a1ea488d888cf2db183f6ac9143c517672
Ray <rraayy246@gmail.com>
2025-09-20 07:45:58 +0000

    test solarized_light

--- home/theme.nix
+++ home/theme.nix
@@ -1,11 +1,11 @@
 let
-  theme = "gruvbox-light";
+  theme = "Solarized (light)";
 in
 {
   programs = {
     bat.config.theme = theme;
-    bottom.settings.flags.color = theme;
+    bottom.settings.flags.color = "gruvbox-light";
     git.delta.options.syntax-theme = theme;
-    helix.settings.theme = "gruvbox_light";
+    helix.settings.theme = "solarized_light";
   };
 }
