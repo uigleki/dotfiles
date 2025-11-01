f076008d6bdf9185885bcfdae49fe0b2353a8799
Ray <rraayy246@gmail.com>
2025-11-01 10:37:45 +0000

    refactor: update security configuration with additional sysctl parameters and cleanup

--- nixos/security.nix
+++ nixos/security.nix
@@ -27,6 +27,30 @@ in
         "oops=panic"
       ];
 
+      kernel.sysctl = {
+        "dev.tty.ldisc_autoload" = 0;
+        "fs.protected_fifos" = 2;
+        "fs.protected_regular" = 2;
+        "fs.suid_dumpable" = 0;
+        "kernel.kexec_load_disabled" = 1;
+        "kernel.kptr_restrict" = 2;
+        "kernel.perf_event_paranoid" = 3;
+        "kernel.sysrq" = 4;
+        "kernel.yama.ptrace_scope" = 2;
+        "net.core.default_qdisc" = "cake";
+        "net.ipv4.conf.all.rp_filter" = 1;
+        "net.ipv4.conf.all.secure_redirects" = 0;
+        "net.ipv4.conf.all.send_redirects" = 0;
+        "net.ipv4.conf.default.accept_redirects" = 0;
+        "net.ipv4.conf.default.secure_redirects" = 0;
+        "net.ipv4.conf.default.send_redirects" = 0;
+        "net.ipv4.tcp_congestion_control" = "bbr";
+        "net.ipv4.tcp_fastopen" = 3;
+        "net.ipv4.tcp_rfc1337" = 1;
+        "net.ipv6.conf.all.accept_redirects" = 0;
+        "net.ipv6.conf.default.accept_redirects" = 0;
+      };
+
       blacklistedKernelModules = [
         "dccp"
         "sctp"
@@ -58,32 +82,6 @@ in
         "ohci1394"
         "sbp2"
       ];
-
-      kernelModules = [ "tcp_bbr" ];
-
-      kernel.sysctl = {
-        "dev.tty.ldisc_autoload" = 0;
-        "fs.protected_fifos" = 2;
-        "fs.protected_regular" = 2;
-        "fs.suid_dumpable" = 0;
-        "kernel.kexec_load_disabled" = 1;
-        "kernel.kptr_restrict" = 2;
-        "kernel.perf_event_paranoid" = 3;
-        "kernel.sysrq" = 4;
-        "kernel.yama.ptrace_scope" = 2;
-        "net.core.default_qdisc" = "cake";
-        "net.ipv4.conf.all.rp_filter" = 1;
-        "net.ipv4.conf.all.secure_redirects" = 0;
-        "net.ipv4.conf.all.send_redirects" = 0;
-        "net.ipv4.conf.default.accept_redirects" = 0;
-        "net.ipv4.conf.default.secure_redirects" = 0;
-        "net.ipv4.conf.default.send_redirects" = 0;
-        "net.ipv4.tcp_congestion_control" = "bbr";
-        "net.ipv4.tcp_fastopen" = 3;
-        "net.ipv4.tcp_rfc1337" = 1;
-        "net.ipv6.conf.all.accept_redirects" = 0;
-        "net.ipv6.conf.default.accept_redirects" = 0;
-      };
     };
   };
 }
