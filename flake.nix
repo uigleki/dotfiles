ab4ccfe42e4375a52e57e82604a7fa32f6a57b4d
Ray <git.lesser278@aleeas.com>
2025-11-18 12:35:53 +0000

    refactor: update home-manager URL to point to release-25.05

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     disko = {
