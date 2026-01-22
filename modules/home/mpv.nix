ffda8f2f50e6aaf2fc18d8a61a563303caf88eca
Ray <30580339+uigleki@users.noreply.github.com>
2026-01-22 10:58:57 +0800

    chore: simplify vscode config and enhance mpv uosc setup - Remove .gitattributes and general editor settings from vscode - Keep only nix-ide and direnv as recommended extensions - Add dependabot for weekly GitHub Actions updates - Configure mpv uosc with custom keybindings for seek, volume, and speed

--- modules/home/mpv.nix
+++ modules/home/mpv.nix
@@ -20,6 +20,10 @@ in
         fullscreen = "yes";
         idle = "once";
         save-position-on-quit = "yes";
+
+        # required by uosc
+        osd-bar = "no";
+        border = "no";
       };
 
       scripts = with pkgs.mpvScripts; [
@@ -27,6 +31,24 @@ in
         thumbfast
         uosc
       ];
+
+      bindings = {
+        right = "seek 5; script-binding uosc/flash-timeline";
+        left = "seek -5; script-binding uosc/flash-timeline";
+        "shift+right" = "seek 30; script-binding uosc/flash-timeline";
+        "shift+left" = "seek -30; script-binding uosc/flash-timeline";
+
+        m = "no-osd cycle mute; script-binding uosc/flash-volume";
+        up = "no-osd add volume 10; script-binding uosc/flash-volume";
+        down = "no-osd add volume -10; script-binding uosc/flash-volume";
+
+        "[" = "no-osd add speed -0.25; script-binding uosc/flash-speed";
+        "]" = "no-osd add speed 0.25; script-binding uosc/flash-speed";
+        "\\" = "no-osd set speed 1; script-binding uosc/flash-speed";
+
+        ">" = "script-binding uosc/next; script-message-to uosc flash-elements top_bar,timeline";
+        "<" = "script-binding uosc/prev; script-message-to uosc flash-elements top_bar,timeline";
+      };
     };
   };
 }
