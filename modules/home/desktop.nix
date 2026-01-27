ac9e37b9e45f24985967e066da3206bb66ec9be1
Ray <30580339+uigleki@users.noreply.github.com>
2026-01-27 11:37:29 +0800

    feat(kitty): add clipboard keybindings and copy-on-select

--- modules/home/desktop.nix
+++ modules/home/desktop.nix
@@ -41,7 +41,14 @@ in
 
       kitty = {
         enable = true;
-        settings.cursor_blink_interval = 0;
+        keybindings = {
+          "ctrl+c" = "copy_or_interrupt";
+          "ctrl+v" = "paste_from_clipboard";
+        };
+        settings = {
+          copy_on_select = "yes";
+          cursor_blink_interval = 0;
+        };
       };
     };
 
