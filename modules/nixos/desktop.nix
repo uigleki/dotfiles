ec874d77e0855503e4f547a59d0a437baf6c4018
Ray <30580339+uigleki@users.noreply.github.com>
2026-05-12 21:55:47 +0800

    chore: enable fcitx5 wayland frontend

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -72,6 +72,7 @@ in
         type = "fcitx5";
 
         fcitx5 = {
+          waylandFrontend = true;
           addons = with pkgs; [
             qt6Packages.fcitx5-chinese-addons
             fcitx5-pinyin-zhwiki
